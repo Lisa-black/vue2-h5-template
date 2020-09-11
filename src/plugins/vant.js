e31db27fae6683215b6172b844bf04d6fc406b8e
骑着蜗牛看日出 <moxcblack@gmail.com>
2020-09-11 15:00:41 +0800

    初始化模板

---
+++ src/plugins/vant.js
@@ -0,0 +1,13 @@
+// 按需全局引入 vant组件
+import Vue from 'vue'
+import { Button, List, Cell, Tabbar, TabbarItem, Row, Col, Icon, CellGroup } from 'vant'
+
+Vue.use(List)
+  .use(Cell)
+  .use(Button)
+  .use(Tabbar)
+  .use(TabbarItem)
+  .use(Row)
+  .use(Col)
+  .use(Icon)
+  .use(CellGroup)
